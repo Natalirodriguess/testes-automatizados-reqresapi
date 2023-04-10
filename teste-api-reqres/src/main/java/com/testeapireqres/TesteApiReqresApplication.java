8584af60b3e7094baa8b5a9ab01c1befd9033df0
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-04-09 21:46:46 -0300

    criando o projeto para iniciar os testes automatizados

---
+++ teste-api-reqres/src/main/java/com/testeapireqres/TesteApiReqresApplication.java
@@ -0,0 +1,13 @@
+package com.testeapireqres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApiReqresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteApiReqresApplication.class, args);
+	}
+
+}
